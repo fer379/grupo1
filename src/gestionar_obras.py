0e78b9b65f43c8b749f0603f3581b22d913cbac5
fer379 <martinfernando29@gmail.com>
2025-11-17 17:12:58 -0300

    prueba

--- src/gestionar_obras.py
+++ src/gestionar_obras.py
@@ -5,3 +5,11 @@ from modelo_orm import db, AreaResponsable, TipoObra, Barrio, Etapa, Empresa, Ti
 from peewee import IntegrityError
 from dateutil import parser
 
+class GestionarObra(ABC):
+    @classmethod
+    def limpiar_datos(cls):
+        cols_text = ['area_responsable', 'tipo_obra', 'barrio', 'empresa', 'fuente_financiamiento', 'etapa']
+        for c in cols_text:
+            if c in df.columns:
+                df[c] = df[c].astype(str).str.strip().replace({'nan': None, 'None': None})
+    
